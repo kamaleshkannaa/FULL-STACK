e649a6d36084b0602e6faf2b838e2ab37bcc38c4
kamaleshkannaa <120234666+kamaleshkannaa@users.noreply.github.com>
2024-05-13 09:13:48 +0530

    Add files via upload

---
+++ resume/script.js
@@ -0,0 +1,58 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+const generateBtn = document.getElementById("generateBtn");
+const previewContent = document.getElementById("preview-content");
+const print = document.getElementById("print");
+
+generateBtn.addEventListener("click", generateResume);
+
+  function generateResume() {
+
+    const nameOfPerson = document.getElementById("nameOfPerson").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+    const location = document.getElementById("location").value;
+    const degree = document.getElementById("degree").value;
+    const university = document.getElementById("university").value;
+    const dateOfGraduation = document.getElementById("dateOfGraduation").value;
+    const jobTitle = document.getElementById("jobTitle").value;
+    const company = document.getElementById("company").value;
+    const skills = document.getElementById("skills").value;
+    const achieve = document.getElementById("achieve").value;
+    const hobby = document.getElementById("hobby").value;
+
+    const resumeHTML = `
+      
+      <h2>${nameOfPerson}</h2>
+      <hr>
+      <p>${email} || ${phone} || ${location}</p>
+      <br>
+
+      <h3>EDUCATION</h3>
+      <p>${degree} - ${university}</p>
+      <p>${dateOfGraduation}</p>
+      
+      <h3>WORK EXPERIENCE</h3>
+      <p>${jobTitle} at ${company}</p>
+
+      <h3>SKILLS</h3>
+      <p>${skills}</p>
+
+      <h3>Achievements</h3>
+      <p>${achieve}</p>
+
+      <h3>Hobbies</h3>
+      <p>${hobby}</p>
+    `;
+
+    previewContent.innerHTML = resumeHTML;
+  }
+
+  print.onclick = () =>{
+    window.print();
+  }
+});
+
+
+
+  
